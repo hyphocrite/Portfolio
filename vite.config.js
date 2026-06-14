94ef39e14f63478b7d9ea55411e1ec9ccac8539a
Felipe <hyphocrite12@gmail.com>
2026-06-14 23:21:23 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 
 // Must match your GitHub repo name: hyphocrite/portfolio → /portfolio/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/Portfolio/',
 });
